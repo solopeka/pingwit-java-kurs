c850023b275a5670f71beea1f031d0c68d1d6214
solopeka <sergeisimonchik@yandex.by>
2023-03-09 19:53:43 +0100

    2nd commit

--- src/main/java/com/pingwit/Main.java
+++ src/main/java/com/pingwit/Main.java
@@ -3,6 +3,6 @@ package com.pingwit;
 public class Main {
     public static void main(String[] args) {
         //Hello
-        System.out.println("Hello world!");
+        System.out.println("Hello world!111111");
     }
 }
