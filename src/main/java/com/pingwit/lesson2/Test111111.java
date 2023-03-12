26dadaf5e0d572dca9dec0c6bcec7fb2035dc4cb
solopeka <sergeisimonchik@yandex.by>
2023-03-12 17:19:19 +0100

    Test2

--- src/main/java/com/pingwit/lesson2/Test111111.java
+++ src/main/java/com/pingwit/lesson2/Test111111.java
@@ -4,6 +4,8 @@ public class Test111111 {
 
     public static void main(String[] args) {
 
-        System.out.println("Test111111");
+        String myTest = "OK";
+
+        System.out.println("myTest");
     }
 }
