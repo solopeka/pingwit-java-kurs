4526819e1787ae04a024dd01e0058f505c6614b0
solopeka <sergeisimonchik@yandex.by>
2023-03-12 13:33:56 +0100

    Homework lesson2

--- src/main/java/com/pingwit/lesson2/Task1.java
+++ src/main/java/com/pingwit/lesson2/Task1.java
@@ -5,11 +5,31 @@ public class Task1 {
     public static void main(String[] args) {
 
         float x = 10.1f;
-        float y = 5.5f;
+        double y = 5.55F;
 
         double sum = x + y;
 
+        double x1 = 20.2F;
+        float y1 = 10.5f;
+
+        double sub = x1 - y1;
+
+        float x2 = 30.3f;
+        float y2 = 20.5f;
+
+        float div = x2 / y2;
+
+        double x3 = 40.4F;
+        double y3 = 30.5F;
+
+        double mul = x3 * y3;
+
 
         System.out.println("Result sum: " + sum);
+        System.out.println("Result sub: " + sub);
+        System.out.println("Result div: " + div);
+        System.out.println("Result mul: " + mul);
+
+
     }
 }
